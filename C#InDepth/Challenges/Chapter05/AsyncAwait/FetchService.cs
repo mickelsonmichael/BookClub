ffa159adb416edd2ffc9fbf8ba53704dec2d5056
Michael Mickelson <mickelsonmichael@gmail.com>
2020-03-05 11:57:25 -0700

    Add a few descriptive comments to FetchService

--- C#InDepth/Challenges/Chapter05/AsyncAwait/FetchService.cs
+++ C#InDepth/Challenges/Chapter05/AsyncAwait/FetchService.cs
@@ -9,12 +9,15 @@ namespace AsyncAwait
     {
         public static async Task<List<T>> GetAll<T>() where T : IFetchable, new()
         {
+            // create an instance so we can get the keyword from it
             var keyword = new T().GetFetchKeyword();
             string fetchUrl = $"https://jsonplaceholder.typicode.com/{keyword}";
 
-            using var httpClient = new HttpClient();
-            string result = await httpClient.GetStringAsync(fetchUrl);
+            using var httpClient = new HttpClient(); // create the client
+            string result = await httpClient.GetStringAsync(fetchUrl)
+                                            .ConfigureAwait(false);
             
+            // convert the JSON into the objects
             return JsonConvert.DeserializeObject<List<T>>(result);
         }
     }
