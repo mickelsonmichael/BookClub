473347e347e8f32b142ae7700ea72b32184042b9
Michael M <mickelsonmichael@gmail.com>
2020-03-20 14:59:17 -0600

    Move the Demos into a new directory

---
+++ C#InDepth/Demos/Chapter04/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Chapter04.Examples;
+
+namespace Chapter04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Dynamics.Demo();
+
+           Arguments.Demo();
+
+           GenericVariance.Demo();
+
+            Console.WriteLine("# Dynamics - JSON");
+
+           string todo = DynamicJson.WhatToDo();
+
+           Console.WriteLine(todo);
+
+           DynamicJson.MakeAPost();
+
+            Console.WriteLine("=====\n#Dynamics - Reflection");
+
+           ReflectionDemo.Compare();
+        }
+    }
+
+    
+}
