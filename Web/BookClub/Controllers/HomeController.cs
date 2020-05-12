ed4865eeaf309b7529a28137c9bf3ce3f15eb0c9
mickelsonmichael <mickelsonmichael@gmail.com>
2020-05-12 14:56:16 -0600

    Remove some extra boilerplate that was unneeded

--- Web/BookClub/Controllers/HomeController.cs
+++ Web/BookClub/Controllers/HomeController.cs
@@ -1,29 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using BookClub.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using BookClub.Models;
+using System.Diagnostics;
 
 namespace BookClub.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
--- Web/BookClub/Program.cs
+++ Web/BookClub/Program.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace BookClub
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -18,9 +12,6 @@ namespace BookClub
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
     }
 }
