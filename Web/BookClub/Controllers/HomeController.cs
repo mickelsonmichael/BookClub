2234f38b0772707748435af6a4743d6aa3ab0b14
Michael Mickelson <mickelsonmichael@gmail.com>
2020-05-12 18:53:56 -0600

    Display books on home page

--- Web/BookClub/Controllers/HomeController.cs
+++ Web/BookClub/Controllers/HomeController.cs
@@ -1,14 +1,16 @@
 ﻿using BookClub.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Repo;
 
 namespace BookClub.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index([FromServices] BookClubDbContext dbContext)
         {
-            return View();
+            dbContext.Database.EnsureCreated();
+            return View(dbContext.Books);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
