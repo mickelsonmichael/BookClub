7a6d32b04a2b7a45e515db36de65c1b336152691
mickelsonmichael <mickelsonmichael@gmail.com>
2020-08-03 13:12:31 -0600

    Example: ValueGenerators

--- Web/BookClub.Repo/Models/Author.cs
+++ Web/BookClub.Repo/Models/Author.cs
@@ -5,6 +5,7 @@ namespace Repo.Models
     public class Author
     {
         public string AuthorId { get; set; }
+        public string FriendlyId { get; private set; }
         public string Name { get; set; }
         public ICollection<Book> Books { get; set; }
     }
