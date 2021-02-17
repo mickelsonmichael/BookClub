87d0230209308fa5eb20b781312d0f99a88527f6
mickelsonmichael <mickelsonmichael@gmail.com>
2021-02-17 08:07:14 -0700

    Add chapter 06 link and extra text to home pagee

--- ReactInAction/demo/src/App.js
+++ ReactInAction/demo/src/App.js
@@ -3,6 +3,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Chapter03 from "./chapter03";
 import Chapter05 from "./chapter05";
+import Chapter06 from "./chapter06";
 import Chapter07 from "./chapter07";
 
 function App() {
@@ -13,16 +14,22 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/chapter-03">Chapter 03</Link>
           <Link to="/chapter-05">Chapter 05</Link>
+          <Link to="/chapter-06">Chapter 06</Link>
           <Link to="/chapter-07">Chapter 07</Link>
         </nav>
         <main>
           <Switch>
             <Route exact path="/">
-              <p>Select a section above</p>
+              <section>
+                <p>Welcome to the Dev Book Club React App Demo! Or DBCRAP!</p>
+
+                <p>Select a Chapter above to view some examples.</p>
+              </section>
             </Route>
 
             <Route path="/chapter-03" component={Chapter03} />
             <Route path="/chapter-05" component={Chapter05} />
+            <Route path="/chapter-06" component={Chapter06} />
             <Route path="/chapter-07" component={Chapter07} />
           </Switch>
         </main>
