1c8b38603b11531fef8cde613db079a69927a22d
mickelsonmichael <mickelsonmichael@gmail.com>
2021-10-25 15:01:31 -0600

    3.4.1 - Enable HTTPS

--- ApiSecurityInAction/dotnet/NatterApi/Program.cs
+++ ApiSecurityInAction/dotnet/NatterApi/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,14 @@ namespace NatterApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
+                    /// Section 3.4.1
+                    /// Enable HTTPS
+                    webBuilder.UseKestrel(kestrel =>
+                    {
+                        kestrel.Listen(IPAddress.Loopback, 4567, listenOptions
+                            => listenOptions.UseHttps("localhost.p12", "changeit"));
+                    });
                 });
     }
 }
--- ApiSecurityInAction/dotnet/NatterApi/Startup.cs
+++ ApiSecurityInAction/dotnet/NatterApi/Startup.cs
@@ -29,10 +29,7 @@ namespace NatterApi
 
             services.AddControllers();
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NatterApi", Version = "v1" });
-            });
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "NatterApi", Version = "v1" }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,10 +52,7 @@ namespace NatterApi
 
             app.UseMiddleware<SecureHeadersMiddleware>();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
