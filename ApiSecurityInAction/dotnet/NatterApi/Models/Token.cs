7798daa400ccacccdf8cdc45bb623bbb5ee1ae50
lyn.m.sawyer@gmail.com <lyn.m.sawyer@gmail.com>
2021-11-03 08:57:20 -0400

    added token object and service

---
+++ ApiSecurityInAction/dotnet/NatterApi/Models/Token.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NatterApi.Models
+{
+    public record Token
+    {
+        public string Subject { get; set; }
+        public DateTime Expiry { get; set; }
+        public List<TokenOptions> Attributes { get; set; }
+
+        public Token()
+        {
+            Attributes = new List<TokenOptions>();
+        }
+    }
+
+    public class TokenOptions
+    {
+        public string Attribute { get; set; }
+    }
+}
---
+++ ApiSecurityInAction/dotnet/NatterApi/Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NatterApi.Models;
+
+namespace NatterApi.Services
+{
+    public class TokenService
+    {
+        public TokenService()
+        {
+
+        }
+
+        public Token CreateToken()
+        {
+            return new Token();
+        }
+
+        public bool ReadToken()
+        {
+            return true;
+        }
+
+        public void DeleteToken()
+        {
+
+        }
+    }
+}
