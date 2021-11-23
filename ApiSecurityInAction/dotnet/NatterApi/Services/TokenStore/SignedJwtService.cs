42a736c1ec5a223d0363df78734821adb88b8d45
Michael Mickelson <mickelsonmichael@gmail.com>
2021-11-22 18:35:47 -0700

    6.2.4 - Read JWT with JWT.NET

--- ApiSecurityInAction/dotnet/NatterApi/Services/TokenStore/SignedJwtService.cs
+++ ApiSecurityInAction/dotnet/NatterApi/Services/TokenStore/SignedJwtService.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.Text.Json;
 using System.Threading.Tasks;
 using JWT.Algorithms;
 using JWT.Builder;
 using Microsoft.AspNetCore.Http;
 using NatterApi.Models.Token;
+using Newtonsoft.Json.Linq;
 
 namespace NatterApi.Services.TokenStore
 {
@@ -23,12 +26,8 @@ namespace NatterApi.Services.TokenStore
                 .WithSecret("my_secret")
                 .Audience("https://localhost:4567")
                 .Subject(token.Username)
+                .AddClaim("attrs", token.Attributes)
                 .ExpirationTime(token.Expiration);
-
-            foreach ((string key, string value) in token.Attributes)
-            {
-                builder.AddClaim(key, value);
-            }
 
             return builder.Encode();
         }
@@ -40,8 +39,33 @@ namespace NatterApi.Services.TokenStore
 
         public Token? ReadToken(HttpContext context, string tokenId)
         {
-            // TODO
-            return null;
+            var jwt = JwtBuilder.Create()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret("my_secret")
+                .Decode<IDictionary<string, object>>(tokenId);
+
+            DateTime exp = DateTime.UnixEpoch
+                .AddMilliseconds(
+                    (long)(double)jwt["exp"]
+                );
+
+            Token token = new(
+                exp,
+                (string)jwt["sub"]
+            );
+
+            foreach (JObject val in ((JArray)jwt["attrs"]).Children<JObject>())
+            {
+                foreach (JProperty prop in val.Properties())
+                {
+                    token.Attributes.Add((
+                        prop.Name,
+                        (string?)prop.Value ?? string.Empty
+                    ));
+                }
+            }
+
+            return token;
         }
     }
 }
