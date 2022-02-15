f20928475b3c8d67f7f8f982ded65c22fb49177c
Michael Mickelson <Michael.Mickelson@nasdaq.com>
2022-02-15 09:28:11 -0700

    Add chapter 04 examples

---
+++ FunctionalProgrammingInC#/Chapter04/Chapter04.Console/BridgingTheGapBetweenActionAndFunc.cs
@@ -0,0 +1,34 @@
+using static System.Console;
+
+using Unit = System.ValueTuple;
+
+namespace Chapter04.Section03.SubSection02;
+
+public static class ActionExtensions
+{
+    public static Func<Unit> ToFunc(this Action action)
+        => () =>
+        {
+            action();
+            return default;
+        };
+
+    public static Func<T, Unit> ToFunc<T>(this Action<T> action)
+        => (t) =>
+        {
+            action(t);
+            return default;
+        };
+}
+
+public static class BridgingTheGapBetweenActionAndFunc
+{
+    public static void Run()
+    {
+        Action act = () => WriteLine("Hello!");
+
+        Func<Unit> f = act.ToFunc();
+
+        f();
+    }
+}
---
+++ FunctionalProgrammingInC#/Chapter04/Chapter04.Console/WritingFunctionsSignatureWithArrowNotation.cs
@@ -0,0 +1,32 @@
+using static System.Console;
+
+namespace Chapter04.Section01.SubSection01;
+
+public static class WritingFunctionsSignaturesWithArrowNotation
+{
+    public static void Run()
+    {
+        // int -> string
+        Func<int, string> oneInputOneOutput = (int i) => i.ToString();
+
+        // () -> string
+        Func<string> noInputOneOutput = () => "hello";
+
+        // int -> ()
+        Action<int> oneInputNoOutput = (int i) => WriteLine($"Give me {i}");
+
+        // () -> ()
+        Action noInputNoOutput = () => WriteLine("Hello, World!");
+
+        // (int, int) -> int
+        Func<int, int, int> twoInputsOneOutput = (int a, int b) => a + b;
+
+        // (string, (bool -> bool)) -> bool
+        Func<string, Func<bool, bool>, bool> higherOrder = (string message, Func<bool, bool> swapper)
+            =>
+        {
+            WriteLine(message);
+            return swapper(true);
+        };
+    }
+}
