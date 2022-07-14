a9ce376641e0d40adb3b4a3cd37fb13fd7083a16
Michael Mickelson <Michael.Mickelson@nasdaq.com>
2022-07-14 09:23:54 -0600

    Progress

--- MicroservicesInDotNet/Mike/ShoppingCartService.AspNet/ShoppingCart/ShoppingCartHandler.cs
+++ MicroservicesInDotNet/Mike/ShoppingCartService.AspNet/ShoppingCart/ShoppingCartHandler.cs
@@ -10,44 +10,28 @@ public static class ShoppingCartHandler
     public static Task<IResult> GetCart(
         [FromServices] IShoppingCartStore shoppingCartStore,
         int userId
-    ) => shoppingCartStore.Get(userId).Map(c => Results.Ok(c));
+    ) => shoppingCartStore.Get(userId)
+                        .Map(c => Results.Ok(c))
+                        .AsTask();
 
-    public async static Task<IResult> PostItems(
+    public static Task<IResult> PostItems(
         [FromServices] IShoppingCartStore shoppingCartStore,
         [FromServices] IProductCatalogStore productCatalogStore,
         int userId,
         [FromBody] int[] productIds
-    )
-    {
-        Option<ShoppingCart> cart = await shoppingCartStore.Get(userId);
-        Validation<Error, IEnumerable<ProductCatalogItem>> products = await productCatalogStore.Get(productIds);
-
-        ShoppingCart c = new ShoppingCart(12345);
-
-
-
-        var r = productCatalogStore.Get(productIds)
-            .Map(
-                either => either
-                    .Map(items => ((ShoppingCart)cart).AddItems(items))
-                    .Map(cart => shoppingCartStore.Save(cart))
-                    .Match(
-                        cart => Results.Ok(cart),
-                        err => Results.StatusCode(500)
-                    )
-        );
-
-        return products
-            .MatchAsync(
-            SuccAsync: async p => (await shoppingCartStore.Save(
-                    p.Fold(
-                        state: (ShoppingCart)cart,
-                        (newCart, item) => newCart.AddItem(item)
-                    )
-                )).Match(
-                    Succ: c => Results.Ok(c),
-                    Fail: err => Results.StatusCode(500)
-                ),
-            Fail: (errors) => Results.NotFound(errors));
-    }
+    ) =>
+    productCatalogStore.Get(productIds)
+        .Bind(valid => valid.Match(
+            Succ: products =>
+                shoppingCartStore.Get(userId)
+                    .Map(c => c.AddItems(products))
+                    .Bind(c =>
+                        shoppingCartStore.Save(c)
+                            .Map(t => t.Match(
+                                Succ: _ => Results.Ok(c),
+                                Fail: Results.StatusCode(500)
+                            ))
+                    ),
+            Fail: err => Results.NotFound(err).AsTask()
+        ));
 }
